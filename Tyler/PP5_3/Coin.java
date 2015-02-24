0f8adc4dac5bb97b724fddb2a5cb7a4b3249206d
Tyler <sylsr9@gmail.com>
2015-02-24 09:54:01 -0700

    PP5_3 coin added

--- Tyler/PP5_3/Coin.java
+++ Tyler/PP5_3/Coin.java
@@ -1,6 +1,34 @@
 package PP5_3;
 
-public class Coin 
-{
 
+public class Coin
+{
+	enum Face{Heads, Tails};
+	Face currentFace;
+	public Coin()
+	{
+		flip();
+	}
+	public void flip()
+	{
+		int randNum=(int) (Math.random()*2);
+		if(randNum==0)
+		{
+			currentFace=Face.Heads;
+		}
+		else
+		{
+			currentFace=Face.Tails;
+		}
+	}
+	public Face isHeads()
+	{
+		return (currentFace=Face.Heads);
+	}
+	public String toString()
+	{
+		return (currentFace=Face.Heads) != null?"Heads": "Tails";
+		
+	}
 }
+
